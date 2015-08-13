65cf60c51f6310c0baadfbd325a7f14f1ec1d22a
Leon Wee <leonwee27@gmail.com>
2015-08-14 01:19:20 +0700

    mailgun

--- lib/send_reset_email.rb
+++ lib/send_reset_email.rb
@@ -2,6 +2,8 @@ require 'rest-client'
 
 class SendResetEmail
 
+  API_KEY = ENV['API_KEY']
+
   attr_reader :user
 
   def initialize(user)
@@ -9,7 +11,7 @@ class SendResetEmail
   end
 
   def self.call(user)
-    RestClient.post "https://api:key-88b3d686bd0073cb21d74139d71fda5f"\
+    RestClient.post "https://api:#{API_KEY}"\
     "@api.mailgun.net/v3/sandboxff8aca0bc0f34b9d9634d15a22e76460.mailgun.org/messages",
     :from => "Mailgun Sandbox <postmaster@sandboxff8aca0bc0f34b9d9634d15a22e76460.mailgun.org>",
     :to => "#{user.email}",
